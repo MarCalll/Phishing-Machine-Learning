bb0abca0346b8fd12efe2d7ac17a7ec7c88ad5a8
Marco <126587897+MarCalll@users.noreply.github.com>
2025-07-15 18:51:56 +0200

    Update Principale.py

--- Principale.py
+++ Principale.py
@@ -2,6 +2,8 @@ import legitimate_cleaner
 import phishing_cleaner
 import pandas as pd
 
+## https://1drv.ms/w/c/b4634b89beb127d2/ERBumWK5kSxPsvVa6Z5QJWoBcDoZhPueE-87AKMkHb8GYw?e=8BI0O2
+
 legitimateDF = legitimate_cleaner.create_legitimate_df()
 phishingDF = phishing_cleaner.create_phishing_df()
 combined_df = pd.concat([legitimateDF, phishingDF], ignore_index=True)
